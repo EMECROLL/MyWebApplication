67d39e8d6921a8001513366cca9b6a9156d70555
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2025-09-07 22:03:19 -0500

    Feat: Items CRUD

---
+++ MyWebApplication/Controllers/ItemsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApplication.Data;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _context.Items.ToListAsync();
+            return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == Id);
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == Id);
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int Id)
+        {
+            var item = await _context.Items.FindAsync(Id);
+            if(item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyWebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        // Entities
+        public DbSet<Item> Items { get; set; }
+    }
+}
